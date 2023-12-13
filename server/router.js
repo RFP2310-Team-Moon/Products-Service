09555173a0eef51700521a56209493091b2dff3a
bzw24 <brucewong24@gmail.com>
2023-12-12 16:44:50 -0800

    set up loader token

--- server/router.js
+++ server/router.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/extensions */
 const router = require('express').Router();
 const controllers = require('./controllers.js');
+require('dotenv').config();
 
+router.route(`/${process.env.LOADER}`).get(process.env.LOADER);
 router.route('/products').get(controllers.products.getProducts);
 router.route('/products/:id').get(controllers.products.getProductInfo);
 router.route('/products/:id/styles').get(controllers.products.getProductStyles);

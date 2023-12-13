1727230f7d43526fc68d5efec9550356c9efe9e2
bzw24 <brucewong24@gmail.com>
2023-12-12 16:48:41 -0800

    edited loader route cb function

--- server/router.js
+++ server/router.js
@@ -3,7 +3,10 @@ const router = require('express').Router();
 const controllers = require('./controllers.js');
 require('dotenv').config();
 
-router.route(`/${process.env.LOADER}`).get(process.env.LOADER);
+router.route(`/${process.env.LOADER}`).get((req, res) => {
+  const loaderToken = process.env.LOADER;
+  res.status(200).json({ loaderToken });
+});
 router.route('/products').get(controllers.products.getProducts);
 router.route('/products/:id').get(controllers.products.getProductInfo);
 router.route('/products/:id/styles').get(controllers.products.getProductStyles);
